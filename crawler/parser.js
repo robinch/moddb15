1dbc2088097b895d33cc2bc739da3d856618952f
Robin Chowdhury <robinch88@gmail.com>
2015-05-24 23:55:55 +0200

    can now read åäö :joy:

--- crawler/parser.js
+++ crawler/parser.js
@@ -5,26 +5,44 @@ var db = require('./database');
 
 // File directory here
 var instream = fs.createReadStream('../page_links_sv.nt');
-var outstream = fs.createWriteStream('out.nq');
+// var outstream = fs.createWriteStream('out.nq');
 var rl = readline.createInterface({
 	input: instream, 
 	// output: outstream,
 	terminal: false});
 
+/*  CHANGE THIS TO INSERT MORE ARTICLES
+    INTO THE DATABASE!!!
+*/
 var NR_OF_ARTICLES = 1000;
 
 
-// Parses the url and return the last
-// word.
-// IF the word has unicode characters or
-// is a file (.*) it returns null
+/*  Parses the url and return the last
+    word.
+    IF the word has unicode characters or
+    is a file (.*) it returns null
+*/
 function getName(s){
-	if( (index = s.search(/\/[\w\(\)]+(?!\.\w+)\>$/i))  > 0 ){
-			return s.substring(index+1, s.length -1);
-		}else{
-			return null;
-		}
+	if( (index = s.search(/\/[\w\(\)åäö]+(?!\.\w+)\>$/i))  > 0 ){
+		return s.substring(index+1, s.length -1);
+	}else{
+		return null;
 	}
+}
+
+
+/*
+    decodes unicode to char
+    ex. \u00E5 to å
+*/
+function decode(s){
+	if(s == null) return null;
+	var r = /\\u([\d\w]{4})/gi;
+	s = s.replace(r, function (match, grp) {
+		return String.fromCharCode(parseInt(grp, 16)); } );
+	s = unescape(s);
+	return s;
+}
 
 /*
 	Takes a from page_links and
@@ -32,42 +50,44 @@ function getName(s){
 	configured after database.js
 */
 function getWikiObject(line){
-	// DOES NOT SUPPORT UNICODE
+	line = decode(line);
 	var split = line.split(" ");
-	s = split[0];
-	var index
-	var parsed;
 	var title = getName(split[0]);
 	var link = getName(split[2]);
-
 	if(!((title == null )|| (link == null))){
 		return {wikiId:title,link:link};
-		}
-		return null;
+	}
+	return null;
 }
 
 function read(){
-var i = 0;
-var wikiObj;
+	var i = 0;
+	var wikiObj;
+
 	rl.on('line', function(line) {
 	  // process line here
 	  if(i < NR_OF_ARTICLES){
-		wikiObj = getWikiObject(line);
-		if(wikiObj != null){
-			console.log(wikiObj)
-			db.linkArticles(wikiObj.wikiId, wikiObj.link);
-		}
+	  	wikiObj = getWikiObject(line);
+	  	if(wikiObj != null){
+	  		db.linkArticles(wikiObj.wikiId, wikiObj.link);
+            // console.log(wikiObj);
+	  	}
+
 	  	i++;
-		}else{
-			close();
-		}
+
+	  }else{
+	  	rl.close();
+	  }
 	});
 
-	rl.on('close', function() {
-	  // do something on finish here
-	});
+    rl.on('close', function() {
+        console.log("Closed the stream");
+        return;
+    });
 }
+
 function close(){
 	rl.close();	
 }
+
 read();
